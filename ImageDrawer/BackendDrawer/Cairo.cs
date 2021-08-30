05e3ed9d5d5a38d3ab9377adad931193195da4bc
adidasdevacademy <53920267+adidasdevacademy@users.noreply.github.com>
2021-08-30 22:46:35 +0300

    preps for refactoring

--- ImageDrawer/BackendDrawer/Cairo.cs
+++ ImageDrawer/BackendDrawer/Cairo.cs
@@ -46,19 +46,29 @@ namespace ImageDrawer
 			{
 				List<Point> coords = new List<Point>();
 				int y = origBitmap.Height * lineNumber / param.LinesCount + origBitmap.Height / (param.LinesCount * 2);
-				coords.Add(new Point(0, y));
-				for (int x = 1; x < origBitmap.Width; x += param.ChunkSize)
-				{
-					Color pixel = origBitmap.GetPixel(x, y);
-					int grayscale = (pixel.R + pixel.G + pixel.B) / 3;
-					int factor = param.Factor * (grayscale - 127) / 127;
-					coords.Add(new Point(x + (int)(factor * Math.Sin(Math.PI * -param.Angle / 180.0)),
-										 y + (int)(factor * Math.Cos(Math.PI * -param.Angle / 180.0))));
-				}
 
-				RenderLine(graphics, coords, param);
+
+
+				if (param.DrawOnSides)
+					coords.Add(CalculatePoint(origBitmap, 0, y, param));
+				for (int x = (origBitmap.Width / 2) % param.ChunkSize; x < origBitmap.Width; x += param.ChunkSize)
+					coords.Add(CalculatePoint(origBitmap, x, y, param));
+				if (param.DrawOnSides)
+					coords.Add(CalculatePoint(origBitmap, origBitmap.Width - 1, y, param));
+
+				RenderLine(graphics, coords, param, y);
 				lineNumber++;
 			}
+		}
+
+		static Point CalculatePoint(Bitmap origBitmap, int x, int y, RenderParams param)
+		{
+			Color pixel = origBitmap.GetPixel(x, y);
+			int grayscale = (pixel.R + pixel.G + pixel.B) / 3;
+			int factor = param.Factor * (grayscale - 127) / 127;
+			return new Point(x + (int)(factor * Math.Sin(Math.PI * -param.Angle / 180.0)),
+							 y + (int)(factor * Math.Cos(Math.PI * -param.Angle / 180.0)));
+
 		}
 
 		static void MethodSquiggle(Graphics graphics, Bitmap origBitmap, RenderParams param)
@@ -84,22 +94,30 @@ namespace ImageDrawer
 					sign *= -1;
 				}
 
-				RenderLine(graphics, coords, param);
+				RenderLine(graphics, coords, param, y);
 				lineNumber++;
 			}
 		}
 
-		private static void RenderLine(Graphics graphics, List<Point> coords, RenderParams param)
+		private static void RenderLine(Graphics graphics, List<Point> coords, RenderParams param, int y)
 		{
 			if (coords.Count == 1)
 				return;
-			Brush brush = new SolidBrush(Color.Black);
+			SolidBrush brush = new SolidBrush(Color.Black);
 			Pen pen = new Pen(brush, param.Stroke);
 
 			switch (param.LineType)
 			{
 				case LineType.Line:
 					DrawLine(graphics, coords.Select(e => e.X).ToArray(), coords.Select(e => e.Y).ToArray());
+					break;
+				case LineType.VariableLine:
+					for (int i = 0; i < coords.Count - 1; i++)
+					{
+						var coord1 = coords[i];
+						var coord2 = coords[i + 1];
+						graphics.FillVariableLine(brush, coord1.X, coord1.Y, coord2.X, coord2.Y, Math.Abs(y - coord1.Y), Math.Abs(y - coord2.Y));
+					}
 					break;
 				case LineType.Curve:
 					graphics.DrawCurve(pen, coords.ToArray());
